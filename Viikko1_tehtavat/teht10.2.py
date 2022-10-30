08a2e51e45223210ecfd7e2f167cdcd69482a619
samwise50000 <sami.mattila3@metropolia.fi>
2022-10-30 23:54:08 +0200

    Tehtavia

--- Viikko1_tehtavat/teht10.2.py
+++ Viikko1_tehtavat/teht10.2.py
@@ -1,10 +1,8 @@
-# Kirjoita Hissi-luokka, joka saa alustajaparametreinaan alimman ja ylimmän kerroksen numeron.
-# Hissillä on metodit siirry_kerrokseen, kerros_ylös ja kerros_alas.
-# Uusi hissi on aina alimmassa kerroksessa.
-# Jos tee luodulle hissille h esimerkiksi metodikutsun h.siirry_kerrokseen(5), metodi kutsuu joko kerros_ylös- tai kerros_alas-metodia niin monta kertaa,
-# että hissi päätyy viidenteen kerrokseen.
-# Viimeksi mainitut metodit ajavat hissiä yhden kerroksen ylös- tai alaspäin ja ilmoittavat, missä kerroksessa hissi sen jälkeen on.
-# Testaa luokkaa siten, että teet pääohjelmassa hissin ja käsket sen siirtymään haluamaasi kerrokseen ja sen jälkeen takaisin alimpaan kerrokseen.
+# Jatka edellisen tehtävän ohjelmaa siten, että teet Talo-luokan.
+# Talon alustajaparametreina annetaan alimman ja ylimmän kerroksen numero sekä hissien lukumäärä.
+# Talon luonnin yhteydessä talo luo tarvittavan määrän hissejä. Hissien lista tallennetaan talon ominaisuutena.
+# Kirjoita taloon metodi aja_hissiä, joka saa parametreinaan hissin numeron ja kohdekerroksen.
+# Kirjoita pääohjelmaan lauseet talon luomiseksi ja talon hisseillä ajelemiseksi.
 
 class Hissi:
     def __init__(self, lowest_floor, highest_floor):

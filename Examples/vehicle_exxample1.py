9131efc70e974e773d5f9fa9f608bd93526a1b28
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-01 11:55:19 +0200

    Tehtavia

--- Examples/vehicle_exxample1.py
+++ Examples/vehicle_exxample1.py
@@ -1,5 +1,6 @@
 class Vehicle:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.speed = 0
         self.odometer = 0
 
@@ -7,13 +8,31 @@ class Vehicle:
         self.speed += 1
 
     def print_info(self):
-        print(f"V1 speed: {self.speed}, odometer: {self.odometer}")
+        print(f"{self.name}: speed: {self.speed}, odometer: {self.odometer}")
+
+class Bike(Vehicle):
+    def __init__(self, name, gears):
+        super().__init__(name)
+        self.number_of_gears = gears
+    def print_info(self):
+        super().print_info()
+        print(f"Number of gears: {self.number_of_gears}")
 
 class Car(Vehicle):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name):
+        super().__init__(name)
         self.gas_level = 30
+    def accelerate(self):
+        super().accelerate()
+        self.speed += 5
+        self.gas_level -= 1
+    def print_info(self):
+        super().print_info()
+        print(f"Gas level: {self.gas_level}")
 
-v1 = Car()
+v1 = Car("Ralliauto")
 v1.accelerate()
 v1.print_info()
+v2 = Bike("Jopo", 1)
+v2.accelerate()
+v2.print_info()

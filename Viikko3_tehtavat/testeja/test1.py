f795f8d943356d20b5758b75ca1e06841150fe74
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-17 13:44:50 +0200

    Tehtavia

---
+++ Viikko3_tehtavat/testeja/test1.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/summa')
+def summa():
+    args = request.args
+    luku1 = float(args.get("luku1"))
+    luku2 = float(args.get("luku2"))
+    summa = luku1+luku2
+
+    vastaus = {
+        "luku1" : luku1,
+        "luku2": luku2,
+        "summa": summa,
+    }
+    return str(vastaus)
+#jos laittaa returniin summan, niin palauttaa summan
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)

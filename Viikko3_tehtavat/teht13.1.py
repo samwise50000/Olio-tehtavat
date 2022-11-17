2de566f6bbe55b53dc844b820aa93d65c061f554
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-17 14:35:57 +0200

    Tehtavia

--- Viikko3_tehtavat/teht13.1.py
+++ Viikko3_tehtavat/teht13.1.py
@@ -2,3 +2,36 @@
 # Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
 # Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
 # Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/alkuluku/<Number>')
+
+
+def alkuluku(Number):
+        Number = float(Number)
+        isPrime = luku_alku(Number)
+
+        vastaus = {
+            "Number": Number,
+            "isPrime": isPrime
+        }
+        return vastaus
+
+def luku_alku(tulos):
+    if tulos > 1:
+        for i in range(2, int(tulos / 2) + 1):
+            if (tulos % i) == 0:
+                return "false"
+        else:
+            return "true"
+
+    # Jos numero on alempi kuin 1, se ei ole alkuluku.
+    else:
+        return "false"
+
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)

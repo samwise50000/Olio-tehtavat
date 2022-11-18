7dc33960d846e4482ad77d5ae02f35d4c2867d62
samwise50000 <sami.mattila3@metropolia.fi>
2022-11-18 13:32:59 +0200

    Tehtavia

--- Viikko3_tehtavat/teht13.2.py
+++ Viikko3_tehtavat/teht13.2.py
@@ -2,3 +2,53 @@
 # Tiedot haetaan opintojaksolla käytetystä lentokenttätietokannasta.
 # Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa:
 # http://127.0.0.1:3000/kenttä/EFHK. Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport", "Municipality":"Helsinki"}.
+from flask import Flask, Response
+import json
+import mysql
+
+import mysql.connector
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='SeOnSiina!?,',
+         autocommit=True
+         )
+
+app = Flask(__name__)
+@app.route('/Kenttä/<Koodi>')
+def Kenttä(koodi):
+    try:
+        koodikysymys = float(koodi)
+        f'select name, municipality from airport where ident = {koodikysymys}'
+        tilakoodi = 200
+        vastaus = {
+            "ICAO": koodikysymys,
+            "Name": Lentokenttä,
+            "Municipality": Kaupunki
+        }
+
+
+
+    except ValueError:
+        tilakoodi = 400
+        vastaus = {
+            "status": tilakoodi,
+            "teksti": "Virheellinen yhteenlaskettava"
+        }
+
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=tilakoodi, mimetype="application/json")
+
+@app.errorhandler(404)
+def page_not_found(virhekoodi):
+    vastaus = {
+        "status" : "404",
+        "teksti" : "Virheellinen päätepiste"
+    }
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=404, mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
